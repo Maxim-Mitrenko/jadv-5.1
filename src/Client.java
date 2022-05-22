ac947f80c75a7d03a3bcc0e9d6ff625cd6cb8aaa
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-05-22 18:48:40 +0300

    Edit Client.java

--- src/Client.java
+++ src/Client.java
@@ -19,6 +19,8 @@ public class Client {
             System.out.println(bufferedReader.readLine());
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            scanner.close();
         }
     }
 
